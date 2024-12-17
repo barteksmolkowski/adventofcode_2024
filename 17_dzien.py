a75669e89f027032f35c5e5923c8cb53d9bcc552
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2024-12-17 12:35:54 +0100

    17 dzien - poprawa kodu

--- 17_dzien.py
+++ 17_dzien.py
@@ -12,8 +12,7 @@ for i in range(0, 3):
         print(f"Błąd w odczycie rejestru {tablica[i]}")
         sys.exit()
 
-instrukcja, instrukcje = tablica[4][9:]
-instrukcje = instrukcja.split(',')
+instrukcje = tablica[4][9:].split(',')
 
 for i in range(len(instrukcje)):
 
@@ -26,42 +25,31 @@ for i in range(len(instrukcje)):
     dane["instrukcja"].append(instrukcje[i]) if i % 2 == 0 else dane["operand"].append(instrukcje[i])
 
 for i in range(len(dane["instrukcja"])):
-
     operand = dane["operand"][i]
-
     match dane["instrukcja"][i]:
-        case 0:
+        case 0:  # A //= 2^operand
             wynik = registery[0] // (2 ** operand)
-            print(f"Instrukcja 0: Rejestr A: {registery[0]} // 2^{operand} = {wynik}")
-
-        case 1:
+            registery[0] = wynik
+        case 1:  # A *= operand
             wynik = registery[0] * operand
-            print(f"Instrukcja 1: Rejestr A: {registery[0]} * {operand} = {wynik}")
-
-        case 2:
+            registery[0] = wynik
+        case 2:  # A += B
             wynik = registery[0] + registery[1]
-            print(f"Instrukcja 2: Rejestr A: {registery[0]} + Rejestr B: {registery[1]} = {wynik}")
-
-        case 3:
-            wynik = registery[2] - operand
-            print(f"Instrukcja 3: Rejestr C: {registery[2]} - {operand} = {wynik}")
-
-        case 4:
+            registery[0] = wynik
+        case 3:  # C -= A + B
+            wynik = registery[2] - (registery[0] + registery[1])
+            registery[2] = wynik
+        case 4:  # suma A, B, C
             wynik = registery[0] + registery[1] + registery[2]
-            print(f"Instrukcja 4: Rejestr A + B + C = {wynik}")
+            print(f"Suma Rejestrów: {wynik}")
+        case 5:  # A //= operand
+            wynik = registery[0] // operand
+            registery[0] = wynik
+        case 7:  # C ^= operand
+            wynik = registery[2] ^ operand
+            registery[2] = wynik
 
-        case 5:
-            tekst = f"Instrukcja 5: Rejestr A: {registery[0]} / {operand} = {wynik}"
-            tekst_blad = "Instrukcja 5: Błąd, dzielenie przez zero."
-            wynik = registery[0] / operand; print(tekst) if operand != 0 else print(tekst_blad)
-
-        case 6:
-            wynik = registery[1] % operand
-            print(f"Instrukcja 6: Rejestr B: {registery[1]} % {operand} = {wynik}")
-
-        case 7:
-            wynik = registery[2] ** operand
-            print(f"Instrukcja 7: Rejestr C: {registery[2]} ^ {operand} = {wynik}")
-
-        case _:
-            print(f"Nieznana instrukcja: {dane['instrukcja'][i]}")
+print("\nWartości rejestrów po wykonaniu programu:")
+print(f"Register A: {registery[0]}")
+print(f"Register B: {registery[1]}")
+print(f"Register C: {registery[2]}")
