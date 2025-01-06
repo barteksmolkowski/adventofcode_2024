14468e05d93f972a06d9d985e323fdee4fcd22da
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2025-01-06 22:28:35 +0100

    dzien 7

--- Dni 2023/07_dzien.py
+++ Dni 2023/07_dzien.py
@@ -3,7 +3,7 @@ gry = {
     "T55J5": "684",
     "KK677": "28",
     "KTJJT": "220",
-    "QQQJA": "483"
+    "QQQJT": "483"
 }
 
 def PoliczZnaki(tekst):
@@ -13,8 +13,60 @@ def PoliczZnaki(tekst):
             znaki[i] = 1
         else:
             znaki[i] += 1
-    print(znaki)
+    return znaki
 
 def Rozpoznaj(gra):
+    znaki = PoliczZnaki(gra)
+    wartosci = sorted(znaki.values(), reverse=True)
     
-    return nazwa # Same5, Same4, Full (11122), trójka, para, różne
+    match wartosci:
+        case [5]: 
+            return 6
+        case [3, 2]: 
+            return 5
+        case [4, 1]:
+            return 4
+        case [3, 1, 1]:
+            return 3
+        case [2, 2, 1]:
+            return 2
+        case [2, 1, 1, 1]:
+            return 1
+        case _:
+            return 0
+
+AlfabetWartosci = list("AKQJT98765432")
+
+def Wartosc(gra1, gra2):
+    rodzaj1, rodzaj2 = Rozpoznaj(gra1), Rozpoznaj(gra2)
+    
+    if rodzaj1 != rodzaj2:
+        return 1 if rodzaj1 > rodzaj2 else -1
+    
+    for karta1, karta2 in zip(gra1, gra2):
+        wartosc1 = AlfabetWartosci.index(karta1)
+        wartosc2 = AlfabetWartosci.index(karta2)
+        if wartosc1 != wartosc2:
+            return 1 if wartosc1 > wartosc2 else -1
+    
+    return 0
+
+gry_lista = list(gry.keys())
+
+for i in range(len(gry_lista)):
+    for j in range(i + 1, len(gry_lista)):
+        if Wartosc(gry_lista[i], gry_lista[j]) < 0:
+            gry_lista[i], gry_lista[j] = gry_lista[j], gry_lista[i]
+
+ranking = {}
+for i in range(len(gry_lista)):
+    ranking[gry_lista[i]] = i + 1
+
+wynik = 0
+for gra in gry_lista:
+    poczatkowa_wygrana = int(gry[gra])
+    wygrana_po_mnozeniu = poczatkowa_wygrana * ranking[gra]
+    print(f"Gra: {gra}, Układ: {Rozpoznaj(gra)}, Miejsce: {ranking[gra]}, Wygrana: {wygrana_po_mnozeniu}")
+    wynik += wygrana_po_mnozeniu
+
+print(f"Łączna wygrana: {wynik}")
